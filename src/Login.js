c6ad054651f22bdd4710372b6582edb9f176a845
Prabal Panda <mynameisprabalpanda@gmail.com>
2021-05-30 17:47:41 +0530

    start

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login
